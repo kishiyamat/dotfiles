fe8e2826234f05170b172949003007e9507091ff
Takeshi KISHIYAMA <kishiyama.t@gmail.com>
2020-11-13 18:59:01 +0900

    open を無効

--- .bash/alias.bash
+++ .bash/alias.bash
@@ -58,21 +58,21 @@ if [ -n "$force_color_prompt" ]; then
     fi
 fi
 
-if [ "$color_prompt" = yes ]; then
-    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
-else
-    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
-fi
+# if [ "$color_prompt" = yes ]; then
+#     PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+# else
+#     PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+# fi
 unset color_prompt force_color_prompt
 
 # If this is an xterm set the title to user@host:dir
-case "$TERM" in
-xterm*|rxvt*)
-    PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
-    ;;
-*)
-    ;;
-esac
+# case "$TERM" in
+# xterm*|rxvt*)
+#     PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
+#     ;;
+# *)
+#     ;;
+# esac
 
 # enable color support of ls and also add handy aliases
 if [ -x /usr/bin/dircolors ]; then
@@ -126,7 +126,7 @@ set -o vi
 
 # nautilus は f で開きたい。
 alias f='nautilus'
-alias open='nautilus'  # Macと同じ...なんだが、mac側を破壊しないかこれ？
+# alias open='nautilus'  # Macと同じ...なんだが、mac側を破壊しないかこれ？
 source $HOME/.git-prompt.sh         # git の prompt を綺麗にしてくれる
 source $HOME/.git-completion.bash   # git の tab補完をしてくれる
 # プロンプトに各種情報を表示
